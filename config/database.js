975bcc04e3fa945ad260c924fb6e62fb32827fc6
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-25 11:31:26 -0400

    adding db

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://devdaph:tPshcQuYG0ltFxR9@cluster0.h8uhl.mongodb.net/todoList?retryWrites=true&w=majority', 
+    'dbName': 'todoList'
+};
